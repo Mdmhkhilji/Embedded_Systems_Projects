3fd0e6e156bf47c5cd45911dd7b122b524330949
Mdmhkhilji <mdmhkhilji@gmail.com>
2026-02-18 16:35:45 +0530

    Create auth.h

---
+++ DD_and_RTOS_Project/Smart_Control_Access/Core/Inc/auth.h
@@ -0,0 +1,15 @@
+/*
+ * auth.h
+ *
+ *  Created on: Feb 18, 2026
+ *      Author: MDMHK
+ */
+
+#ifndef AUTH_H
+#define AUTH_H
+
+#include <stdint.h>
+
+uint16_t Authenticate(uint8_t *uid);
+
+#endif
